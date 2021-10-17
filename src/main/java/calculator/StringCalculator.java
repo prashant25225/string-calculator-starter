46e584a37d25aaa11ad77a07ed0557a4c2008f30
Prashant <prashant25225@gmail.com>
2021-10-18 00:21:44 +0530

    Added condition in add method to handle single number case.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,16 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+        // If input is blank
+        if (input == null || input.trim().equals("")) {
+            return 0;
+        }
+
+        // If input contains only single number
+        if (!input.contains(",")) {
+            return Integer.parseInt(input);
+        }
+
         return 0;
     }
 
